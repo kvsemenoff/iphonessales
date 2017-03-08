844dfd0b5693fb9d0c52223b3d0ebe039e1c5a46
Berzeg07 <berezg86@gmail.com>
2017-03-08 16:53:12 +0300

    29.14

--- includes/original.php
+++ includes/original.php
@@ -1,8 +1,78 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<!-- Шапка -->
+
+<section class="dd-head-wrap">
+	<div class="dd-container dd-relative">
+		<div class="dd-txt_h">
+			<span>Оригинальные</span>
+			<span>Apple iPhone</span>
+			<span>в Санкт-Петербурге</span>
 		</div>
-	</div>
-</div>
-</section>
+		<div class="dd-txt_h dd-txt_h_big">
+			<span>Оригинальные <span>Apple iPhone</span></span>
+			<span>в Санкт-Петербурге</span>
+		</div>
+		<div class="dd-main_h">
+			<span class="dd-sp1_h">Закажите
+				прямо сейчас
+			</span>
+			<span class="dd-sp2_h">и получите защитное
+				стекло в подарок!
+			</span>
+			<form action="#" method="post">
+				<div class="dd-form-box_h">
+					<input class="dd-input" type="text" name="uname" placeholder="Имя">
+					<input class="dd-input" type="email" name="phone" placeholder="Номер телефона">
+				</div>
+				<div class="dd-form-box_h">
+					<div  class="az-select">
+						<select name="" id="">
+							<option value="1">Выберите модель</option>
+							<option value="2">iphone 5</option>
+							<option value="3">iphone 6</option>
+							<option value="4">iphone 7</option>
+						</select>
+					</div>
+					<div class="dd-button_h">
+						<a class="dd-button-1 dd-w100" href="#">Заказать телефон</a>
+					</div>
+					<div class="dd-strelka">
+						<img src="img/strel.png" alt="">
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</form>
+
+			<div class="dd-timer">
+				<div class="dd-timer-title">
+					<span>До конца акции осталось:</span>
+				</div>
+				<div class="dd-times">
+					<div class="dd-tochki">
+						<img src="img/tochki.png" alt="">
+					</div>
+					<div class="dd-div dd-first-div">
+						<span id="dd-hours"></span>
+					</div>
+					<div class="dd-div">
+						<span id="dd-min"></span>
+					</div>
+					<div class="dd-div">
+						<span id="dd-sec"></span>
+					</div>
+					<div class="dd-time-txt">
+						<span>часов</span>
+					</div>
+					<div class="dd-time-txt dd-time-txt2">
+						<span>минут</span>
+					</div>
+					<div class="dd-time-txt dd-time-txt3">
+						<span>секунд</span>
+					</div>
+				</div>
+			</div><!-- timer -->
+
+		</div><!-- dd-main_h -->
+	</div><!-- container -->
+</section><!-- section -->
+
+<!-- Конец шапка -->
--- js/common.js
+++ js/common.js
@@ -102,7 +102,101 @@ $(document).ready(function(){
 
 
     $(".phone").mask("+ 7 (999) 999 - 99 - 99?");
+    var count = 8;
+    var minutes = 55;
+    var hours = 10;
 
+    var end = 0;
+
+    if (count < 10) {
+        $("#dd-sec").html('0'+count);
+    }
+    else {
+        $("#dd-sec").html(count);
+    }
+    if (minutes < 10) {
+        $("#dd-min").html('0'+minutes);
+    }
+    else {
+        $("#dd-min").html(minutes);
+    }
+    if (hours < 10) {
+        $("#dd-hours").html('0'+hours);
+    }
+    else {
+        $("#dd-hours").html(hours);
+    }
+
+    var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+    function timer()
+    {
+        count = count - 1;
+        if (count<10) {
+            $("#dd-sec").html('0'+count);
+        } else {
+            $("#dd-sec").html(count);
+        }
+        if (count == 0) {
+            minutes = minutes - 1;
+            if (minutes < 10 && minutes >= 0) {
+                $("#dd-min").html('0'+minutes);
+            }
+            if (minutes > 10) {
+                $("#dd-min").html(minutes);
+            }
+            if (minutes < 0) {
+                hours = hours - 1;
+                if (hours < 10 && hours >=0) {
+                    $("#dd-hours").html('0'+hours);
+                }
+                if (hours>10) {
+                    $("#dd-hours").html('0'+hours);
+                }
+                if (hours < 0 ) {
+                    
+                    end = 1;
+                    clearTimeout(counter);
+                }
+                if (end) { minutes = '00';}
+                else {
+                    minutes = 59;
+                }
+                $("#dd-min").html(minutes);
+            }
+            if (end) { count = '00';}
+            else {
+                count = 59;
+            }
+            
+            $("#dd-sec").html(count);
+        }
+    }
+        var owl = $("#slider");
+    owl.owlCarousel({
+        loop:true,
+        nav:true, 
+        autoplay:false,
+        smartSpeed:1000,
+        margin:25,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="nav-left"></span>','<span class="nav-right"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:1 
+            },
+            990:{
+                items:1
+            },      
+
+            1248:{
+                items:1
+            }
+        }
+    });
 
  });
 
