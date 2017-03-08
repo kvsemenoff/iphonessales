e7c54876f921a1a71aee80be3373711d260477db
engine77 <zaur.1kager@gmail.com>
2017-03-08 11:25:42 +0300

    model

--- includes/model.php
+++ includes/model.php
@@ -1,8 +1,350 @@
 <section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+	<div class="dfcontainer">
+		<h2 class="dfmodcap">Модели iPhone</h2>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatright">
+				<img src="img/iph1.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dfrighttreyg">
+				<h3 class="dfnamemodel">iPhone 6s</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">
+						<a href="#" class="dfc1"></a>
+						<a href="#" class="dfc2"></a>
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">31 990 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatleft">
+				<img src="img/iph2.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dflefttreyg">
+				<h3 class="dfnamemodel">iPhone 6</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">						
+						<a href="#" class="dfc2"></a>
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">23 990 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatright">
+				<img src="img/iph1.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dfrighttreyg">
+				<h3 class="dfnamemodel">iPhone 6 Plus</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">						
+						<a href="#" class="dfc2"></a>
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">41 990 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatleft">
+				<img src="img/iph3.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dflefttreyg">
+				<h3 class="dfnamemodel">iPhone 5s</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">						
+						<a href="#" class="dfc2"></a>
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">14 990 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatright">
+				<img src="img/iph4.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dfrighttreyg">
+				<h3 class="dfnamemodel">iPhone 5c</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">						
+						
+						<a href="#" class="dfgreen"></a>
+						<a href="#" class="dfyellow"></a>
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfblue"></a>						
+						<a href="#" class="dfc1"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">10 990 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatleft">
+				<img src="img/iph5.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dflefttreyg">
+				<h3 class="dfnamemodel">iPhone 5</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">								
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">11 490 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatright">
+				<img src="img/iph6.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dfrighttreyg">
+				<h3 class="dfnamemodel">iPhone 4s</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">						
+						<a href="#" class="dfc3"></a>									
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">7 290 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
+		</div>
+		<div class="dfmodel">
+			<div class="dfmodelimg floatleft">
+				<img src="img/iph7.png" alt="" class="img-responsive">
+			</div>	
+			<div class="dfmodelchar dflefttreyg">
+				<h3 class="dfnamemodel">iPhone 4</h3>
+				<div class="dfcharblock">
+				<div class="dfcharbox">
+					<span>Размеры:</span>
+					<span>67,1 x 138,3 x 7,1 мм</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Масса:</span>
+					<span>143 г</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Обьём встроенной памяти:</span>
+					<span>16 Gb</span>
+				</div>
+				<div class="dfcharbox">
+					<span>Операционная система: </span>
+					<span>iOS 8</span>
+				</div>
+				<div class="dfcharram">
+					<span class="dfobe">Объём памяти:</span>
+					<div class="dfgb"><a href="#">16Gb</a><a href="#"> 64Gb</a><a href="#">128Gb</a></div>
+				</div>
+				<div class="dfcharbox dfcharboxlast">
+					<span>Цвета:</span>
+					<div class="dfcolor">								
+						<a href="#" class="dfc3"></a>
+						<a href="#" class="dfc4"></a>
+					</div>
+				</div>
+				</div>
+
+				<h4 class="dfprice">5 490 <span>руб.</span></h4>
+				<a href="#" class="dfbron">Забронировать</a>
+				<a href="#" class="dfras">В кредит</a>
+			</div>
+			<div class="clear"></div>			
 		</div>
 	</div>
-</div>
 </section>
