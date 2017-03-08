68433b2ca53dbcd880411a4db8ff966cc776e005
Berzeg07 <berezg86@gmail.com>
2017-03-08 13:56:55 +0300

    29.14

--- berzeg.php
+++ berzeg.php
@@ -176,31 +176,65 @@
 		</div>
 
 		<div class="dd-main_h">
-			<span>Закажите
+			<span class="dd-sp1_h">Закажите
 				прямо сейчас
 			</span>
-			<span>и получите защитное
+			<span class="dd-sp2_h">и получите защитное<br>
 				стекло в подарок!
 			</span>
 			<form action="#" method="post">
-				<input type="text" name="uname" placeholder="Имя">
-				<input type="text" name="phone" placeholder="Номер телефоны">
-				<div  class="az-select">
-					<select name="" id="">
-						<option value="1">Выберите модель</option>
-						<option value="2">iphone 5</option>
-						<option value="3">iphone 6</option>
-						<option value="4">iphone 7</option>
-					</select>
+				<div class="dd-form-box_h">
+					<input class="dd-input" type="text" name="uname" placeholder="Имя">
+					<input class="dd-input" type="email" name="phone" placeholder="Номер телефона">
 				</div>
-				<div class="dd-button_h">
-					<a href="#">Заказать телефон</a>
+				<div class="dd-form-box_h">
+					<div  class="az-select">
+						<select name="" id="">
+							<option value="1">Выберите модель</option>
+							<option value="2">iphone 5</option>
+							<option value="3">iphone 6</option>
+							<option value="4">iphone 7</option>
+						</select>
+					</div>
+					<div class="dd-button_h">
+						<a class="dd-button-1 dd-w100" href="#">Заказать телефон</a>
+					</div>
 				</div>
+				<div class="clearfix"></div>
 			</form>
-		</div>
-		
-	</div>
-</section>
+
+			<div class="dd-timer">
+				<div class="dd-timer-title">
+					<span>До конца акции осталось:</span>
+				</div>
+				<div class="dd-times">
+					<div class="dd-tochki">
+						<img src="img/tochki.png" alt="">
+					</div>
+					<div class="dd-div dd-first-div">
+						<span id="dd-hours"></span>
+					</div>
+					<div class="dd-div">
+						<span id="dd-min"></span>
+					</div>
+					<div class="dd-div">
+						<span id="dd-sec"></span>
+					</div>
+					<div class="dd-time-txt">
+						<span>часов</span>
+					</div>
+					<div class="dd-time-txt dd-time-txt2">
+						<span>минут</span>
+					</div>
+					<div class="dd-time-txt dd-time-txt3">
+						<span>секунд</span>
+					</div>
+				</div>
+			</div><!-- timer -->
+
+		</div><!-- dd-main_h -->
+	</div><!-- container -->
+</section><!-- section -->
 
 <!-- Конец шапка -->
 
@@ -235,7 +269,81 @@
         }
     });
 </script>
+<script>
 
+		var count = 8;
+		var minutes = 55;
+		var hours = 10;
+		
+		var end = 0;
+		
+		if (count < 10) {
+			$("#dd-sec").html('0'+count);
+		}
+		else {
+			$("#dd-sec").html(count);
+		}
+		if (minutes < 10) {
+			$("#dd-min").html('0'+minutes);
+		}
+		else {
+			$("#dd-min").html(minutes);
+		}
+		if (hours < 10) {
+			$("#dd-hours").html('0'+hours);
+		}
+		else {
+			$("#dd-hours").html(hours);
+		}
+		
+	var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+	function timer()
+	{
+		count = count - 1;
+		if (count<10) {
+			$("#dd-sec").html('0'+count);
+		} else {
+			$("#dd-sec").html(count);
+		}
+		if (count == 0) {
+			minutes = minutes - 1;
+			if (minutes < 10 && minutes >= 0) {
+				$("#dd-min").html('0'+minutes);
+			}
+			if (minutes > 10) {
+				$("#dd-min").html(minutes);
+			}
+			if (minutes < 0) {
+				hours = hours - 1;
+				if (hours < 10 && hours >=0) {
+					$("#dd-hours").html('0'+hours);
+				}
+				if (hours>10) {
+					$("#dd-hours").html('0'+hours);
+				}
+				if (hours < 0 ) {
+					
+					end = 1;
+					clearTimeout(counter);
+				}
+				if (end) { minutes = '00';}
+				else {
+					minutes = 59;
+				}
+				$("#dd-min").html(minutes);
+			}
+			if (end) { count = '00';}
+				else {
+					count = 59;
+				}
+			
+			$("#dd-sec").html(count);
+		}
+	}
+
+	
+</script>
 </body>
 </html>
 
