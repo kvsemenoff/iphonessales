bae14c377395821cbdfeae07159e6fc83c51f657
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-03-08 16:02:35 +0600

    completed header-top.php

--- includes/header-top.php
+++ includes/header-top.php
@@ -1,66 +1,77 @@
 <section class="section section_padding head-top">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="top-line clearfix">
-				<div class="menu-burger clearfix">
-					<div class="menu-button">
-						<i class="bg"></i>
-						<i class="bg-close">Скрыть меню</i>
+<div class="inner">
+	<div class=" container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="top-line clearfix">
+					<div class="menu-burger clearfix">
+						<div class="menu-button">
+							<i class="bg"></i>
+							<i class="bg-close">Скрыть меню</i>
+						</div>
+						<nav class="menu-up">
+							<ul class="menu__list">
+								<li class="menu__li"><a href="#">iPhone 6S</a></li>
+								<li class="menu__li"><a href="#">iPhone 6</a></li>
+								<li class="menu__li"><a href="#">iPhone 6Plus</a></li>
+								<li class="menu__li"><a href="#">iPhone 5S</a></li>
+								<li class="menu__li"><a href="#">iPhone 5C</a></li>
+								<li class="menu__li"><a href="#">iPhone 5</a></li>
+								<li class="menu__li"><a href="#">iPhone 4s</a></li>
+								<li class="menu__li"><a href="#">iPhone 4</a></li>
+								<li class="menu__li"><a href="#">Преимущества</a></li>
+								<li class="menu__li"><a href="#">Способы оплаты</a></li>
+								<li class="menu__li"><a href="#">Бонусы</a></li>
+							</ul>
+						</nav>
 					</div>
-					<nav class="menu-up">
-						<ul class="menu__list">
-							<li class="menu__li"><a href="#">iPhone 6S</a></li>
-							<li class="menu__li"><a href="#">iPhone 6</a></li>
-							<li class="menu__li"><a href="#">iPhone 6Plus</a></li>
-							<li class="menu__li"><a href="#">iPhone 5S</a></li>
-							<li class="menu__li"><a href="#">iPhone 5C</a></li>
-							<li class="menu__li"><a href="#">iPhone 5</a></li>
-							<li class="menu__li"><a href="#">iPhone 4s</a></li>
-							<li class="menu__li"><a href="#">iPhone 4</a></li>
-							<li class="menu__li"><a href="#">Преимущества</a></li>
-							<li class="menu__li"><a href="#">Способы оплаты</a></li>
-							<li class="menu__li"><a href="#">Бонусы</a></li>
-						</ul>
-					</nav>
+					<div class="logo">
+						<a href="#"></a>
+					</div>
+					<button class="phone-btn"></button>
+					<div class="right clearfix">
+						<nav class="main-nav">
+							<ul>
+								<li><a href="#">Преимущества</a></li>
+								<li><a href="#">Способы оплаты</a></li>
+								<li><a href="#">Бонусы</a></li>
+								<li><a href="#">Отзывы</a></li>
+								<li><a href="#">Контакты</a></li>
+							</ul>
+						</nav>
+						<div class="phone">
+							<div>
+								<span class="big">8 (800) 775-43-78</span>
+							</div>
+							<div>
+								<span class="sm">8 (812) 385-43-78</span>
+							</div>
+						</div>
+						<div class="btn-wrap">
+							<a href="#" class="button1 button1-pink">Обратный звонок</a>
+						</div>
+					</div>
 				</div>
-				<div class="logo">
-					<a href="#"></a>
-				</div>
-				<nav class="main-nav">
-					<ul>
-						<li><a href="#">Преимущества</a></li>
-						<li><a href="#">Способы оплаты</a></li>
-						<li><a href="#">Бонусы</a></li>
-						<li><a href="#">Отзывы</a></li>
-						<li><a href="#">Контакты</a></li>
-					</ul>
-				</nav>
-				<div class="phone">
-					<span class="big">8 (800) 775-43-78</span>
-					<span class="small">8 (812) 385-43-78</span>
-				</div>
-				<a href="#" class="button1 button1-pink">Забронировать</a>
 			</div>
 		</div>
 	</div>
-</div>
-<div class="nav-product">
-	<div class="menu-g">
-		<div class="container">
-			<nav class="menu">
-				<ul class="menu__list clearfix">
-					<li class="menu__li"><a href="#">iPhone 6S</a></li>
-					<li class="menu__li"><a href="#">iPhone 6</a></li>
-					<li class="menu__li"><a href="#">iPhone 6Plus</a></li>
-					<li class="menu__li"><a href="#">iPhone 5S</a></li>
-					<li class="menu__li"><a href="#">iPhone 5C</a></li>
-					<li class="menu__li"><a href="#">iPhone 5</a></li>
-					<li class="menu__li"><a href="#">iPhone 4s</a></li>
-					<li class="menu__li"><a href="#">iPhone 4</a></li>
-				</ul>
-			</nav>
+	<div class="nav-product">
+		<div class="menu-g">
+			<div class="container">
+				<nav class="menu">
+					<ul class="menu__list clearfix">
+						<li class="menu__li"><a href="#">iPhone 6S</a></li>
+						<li class="menu__li"><a href="#">iPhone 6</a></li>
+						<li class="menu__li"><a href="#">iPhone 6Plus</a></li>
+						<li class="menu__li"><a href="#">iPhone 5S</a></li>
+						<li class="menu__li"><a href="#">iPhone 5C</a></li>
+						<li class="menu__li"><a href="#">iPhone 5</a></li>
+						<li class="menu__li"><a href="#">iPhone 4s</a></li>
+						<li class="menu__li"><a href="#">iPhone 4</a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
-	</div>
-</div><!-- menu - end -->
+	</div><!-- menu - end -->
+</div>
 </section>
