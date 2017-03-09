c49f30a61995582acd97d9db8160bfc4d20b7d4d
ibragimtokmak <tokmakovibragim@gmail.com>
2017-03-09 16:50:27 +0300

    payment header fix

--- includes/leave.php
+++ includes/leave.php
@@ -6,9 +6,9 @@
 		        <h2 class="h2-white leave-title">Оставьте заявку</h2>
 		        <h3 class="leave-subtitle">на покупку телефона <span class="leave-span">в кредит</span></h3>
 		        <p class="leave-managers-md hidden-xs hidden-sm">Наши менеджеры позвонят Вам в течение 15 минут</p>
-		        <form action="#" class="leave-form">
-                    <input type="text" class="leave-input" placeholder="Имя">
-                    <input type="text" class="leave-input phone" placeholder="Номер телефона">
+		        <form action="#" class="leave-form js-submit">
+                    <input type="text" class="leave-input" name="uname" placeholder="Имя">
+                    <input type="text" class="leave-input phone" name="phone" placeholder="Номер телефона">
                     <input type="submit" class="leave-submit" value="Отправить заявку">
 		        </form>
 		        <p class="leave-managers hidden-md hidden-lg">Наши менеджеры позвонят Вам в течение 15 минут</p>
